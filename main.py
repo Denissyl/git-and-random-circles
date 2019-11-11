be4698dfdd7d733af070f9d9795bfcca3a422440
Denis <Denis-syl3@yandex.ru>
2019-11-11 19:26:25 +0500

    Implement random circles generation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+from random import randint
 
-from PyQt5 import uic
-from PyQt5.QtGui import QPainter
+from PyQt5 import uic, QtGui
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.uic.properties import QtGui
+
+from Circle import Circle
 
 
 class YellowCirclesForm(QWidget):
@@ -20,17 +22,27 @@ class YellowCirclesForm(QWidget):
     def init_ui(self):
         pass
 
+    def generate_circles(self):
+        self.circles.clear()
+        for _ in range(10):
+            self.circles.append(Circle(randint(0, self.width()),
+                                       randint(0, self.height()),
+                                       randint(10, 100)))
+
     def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
         self.painter.begin(self)
         self.draw_circles()
         self.painter.end()
 
+
     def draw_circles(self):
+        self.painter.setPen(QColor(255, 200, 0))
         for circle in self.circles:
             self.draw_circle(circle)
 
     def draw_circle(self, circle):
         self.painter.drawEllipse(circle.x, circle.y, circle.r, circle.r)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
