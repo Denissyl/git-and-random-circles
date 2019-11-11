81b6ab1985bc5479a3947d8f691b11c85c29d3a0
Denis <Denis-syl3@yandex.ru>
2019-11-11 19:16:26 +0500

    Implement circles drawing

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.uic.properties import QtGui
 
 
 class YellowCirclesForm(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('Ui.ui', self)
+
+        self.circles = []
+
+        self.painter = QPainter()
+
         self.init_ui()
 
     def init_ui(self):
         pass
 
+    def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
+        self.painter.begin(self)
+        self.draw_circles()
+        self.painter.end()
+
+    def draw_circles(self):
+        for circle in self.circles:
+            self.draw_circle(circle)
+
+    def draw_circle(self, circle):
+        self.painter.drawEllipse(circle.x, circle.y, circle.r, circle.r)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
